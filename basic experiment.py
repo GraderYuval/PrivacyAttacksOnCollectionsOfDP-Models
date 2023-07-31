46cabaaad389935c1697b43cfeca03909753b986
shlomo-tauber <shlomo.tauber@gmail.com>
2023-07-31 13:54:08 +0300

    basic structure of training and experiment.

---
+++ basic experiment.py
@@ -0,0 +1,14 @@
+
+
+def train_models(epsilon, n, data):
+    """
+    Train n models to be epsilon-DP, using the same training set for all models.
+    :param epsilon:
+    :return:
+    """
+
+def check_average():
+    """
+    This experiment check the basic attack when take the average of the attack of the models.
+    :return:
+    """
---
+++ training.py
@@ -0,0 +1,8 @@
+def train_model(epsilon, data):
+    """
+    The function train ResNet model, with epsilon-DP.
+    :param epsilon:
+    :param data:
+    :return:
+    """
+    pass
